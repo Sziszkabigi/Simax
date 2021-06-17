efc9cb5ec7930b6f94b2218ca70a6da90ce44a85
Brigitta Szikszai <55708807+Sziszkabigi@users.noreply.github.com>
2021-06-17 22:24:25 +0100

    add css for hero

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,48 @@ function Header() {
   return (
     <header>
       <logo>
-        <img src="./assets/simax_mapmark_normal.svg" alt="logo" width="40px" />
+        <img
+          className="logo-image"
+          src="./assets/simax_mapmark_normal.svg"
+          alt="logo"
+          width="40px"
+        />
         <span>Simax </span>
       </logo>
       <nav>
         <ul className="menu">
-          <li className="menu-item"> 1 </li>
-          <li className="menu-item"> 2 </li>
-          <li className="menu-item"> 3 </li>
+          <li className="menu-item">
+            <a className="menu-link" href="" target="_self" title="FOOLDAL">
+              FOOLDAL{" "}
+            </a>
+          </li>
+          <li className="menu-item">
+            <a className="menu-link" href="" target="" title="SITEREPEK">
+              SITEREPEK{" "}
+            </a>
+          </li>
+          <li className="menu-item">
+            <a className="menu-link" href="" target="" title="TUDNIVALOK ">
+              TUDNIVALOK{" "}
+            </a>
+          </li>
+          <li className="menu-item">
+            <a className="menu-link" href="" target="" title="BIZTOSITAS">
+              BIZTOSITAS{" "}
+            </a>
+          </li>
+          <li className="menu-item">
+            <a className="menu-link" href="" target="_self" title="BIZTOSITAS">
+              BIZTOSITAS{" "}
+            </a>
+          </li>
         </ul>
       </nav>
+      <div className="hero-content">
+        <h1>Szuperieur lejtok </h1>
+        <h1>Szuperieur aron. </h1>
+        <p> Csuszik a francia? Sielj velunk!</p>
+      </div>
     </header>
   );
 }
