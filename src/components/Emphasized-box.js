972299bfd1a48e7f5a85fee149f0ba8f81e1b7a8
Brigitta Szikszai <55708807+Sziszkabigi@users.noreply.github.com>
2021-06-18 23:18:24 +0100

    add the footer elements

--- src/components/Emphasized-box.js
+++ src/components/Emphasized-box.js
@@ -2,9 +2,27 @@ import { React } from "react";
 
 function EmphasizedBox() {
   return (
-    <div className="emphasized-box">
-      <img src="" alt="" />
-      <p> Advertisement card </p>
+    <div className="emphasized-container">
+      <div className="emphasized-box">
+        <img className="emphasized-image" src="./assets/hero.jpg" alt="" />
+        <div>
+          <h1> Advertisement card </h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electroni
+          </p>
+          <button
+            classNAme="emp-b"
+            type="button"
+            class="btn btn-outline-secondary"
+          >
+            Secondary
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,23 @@ import { React } from "react";
 function Footer() {
   return (
     <footer>
-      <div>
-        <p> Lrem ipsum </p>
-        <p> Email part </p>
+      <div className="footer-top">
+        <h1>Maradj Fokuszban</h1>
+        <p>
+          Szeretnel idoben ertesulni a kedvenc szallashelyeidrol? NE maradj
+          le....
+        </p>
+        <form>
+          <label>
+            <input type="text" email="email" placeholder="Email cimed..." />
+          </label>
+          <input type="submit" value="Kuldes" />
+        </form>
       </div>
-      <div>
-        <p> footer part </p>
+      <div className="footer-bottom">
+        <p>valami 1</p>
+        <p>valami 2</p>
+        <p>valami 3</p>
       </div>
     </footer>
   );
